3363fc07d991ad04d661e8471b16130464e5ab97
Dokyu Lee <leewan@Dokyuui-MacBookPro.local>
2022-07-04 02:52:07 +0900

    Cornucopias 가격 가져오기 - input box에 값 넣기, 지우기 반복으로 구성 - 값을 넣고 지울 때 생기는 변경점을 WebDriverWait으로 구현   1. Presence_of_element_located X   2. visibility_of_element_located X   3. staleness_of O     - DOM에 연결될 때 까지 대기하는 옵션
    값을 넣으면 목록들이 순간 사라졌다가 나타나는데, 1,2번을 시도했지만 통하지 않음

--- selenium_prac.py
+++ selenium_prac.py
@@ -1,4 +1,6 @@
 import time
+from openpyxl import Workbook
+import openpyxl
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
@@ -7,21 +9,30 @@ from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
 CHROME_DRIVER_PATH = '/Users/leewan/driver/chromedriver'
+LUKA7 = 'https://luka7.net'
+ADA_FILE = 'ADA_finance.xlsx'
 
 chrome_options= webdriver.ChromeOptions()
 chrome_options.add_argument("headless")
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
-driver.get('https://luka7.net')
+driver.get(LUKA7)
 WebDriverWait(driver, 100).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#coinList > div.row.val.ADA > div.krw")))
+wb = openpyxl.load_workbook(ADA_FILE)
+sheet = wb.active
 while(1):
     time.sleep(3)
     USD = driver.find_element(By.ID, 'USDKRW').text
+    ADA_KRW = driver.find_element(By.XPATH, '//*[@id="coinList"]/div[6]/div[2]').text
+    '''
     coinList = driver.find_element(By.ID, 'coinList')
     #ADA_KRW = ADA.find_element(By.CLASS_NAME, 'krw')
     ADA = coinList.find_element(By.CLASS_NAME, 'ADA')
-    print(ADA.find_element(By.CLASS_NAME, 'krw').text)
-
+    ADA_KRW = ADA.find_element(By.CLASS_NAME, 'krw').text
+    '''
+    print(ADA_KRW)
+    sheet[ 'A1' ].value = ADA_KRW
+    wb.save(ADA_FILE)
     #print(ADA_USD)
 
 driver.quit()
