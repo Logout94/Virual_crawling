1f197cf5300e84f30a0cc046ce86c22702817255
Dokyu Lee <leewan@Dokyuui-MacBookPro.local>
2022-07-03 16:17:33 +0900

    sleep 간격으로 데이터 가져오기 성공

--- selenium_prac.py
+++ selenium_prac.py
@@ -1,14 +1,27 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
 CHROME_DRIVER_PATH = '/Users/leewan/driver/chromedriver'
 
 chrome_options= webdriver.ChromeOptions()
+chrome_options.add_argument("headless")
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-driver.implicitly_wait(3)
 
 driver.get('https://luka7.net')
-USD = driver.find_element(By.ID, 'USDKRW')
-print(USD.text)
+WebDriverWait(driver, 100).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#coinList > div.row.val.ADA > div.krw")))
+while(1):
+    time.sleep(3)
+    USD = driver.find_element(By.ID, 'USDKRW').text
+    coinList = driver.find_element(By.ID, 'coinList')
+    #ADA_KRW = ADA.find_element(By.CLASS_NAME, 'krw')
+    ADA = coinList.find_element(By.CLASS_NAME, 'ADA')
+    print(ADA.find_element(By.CLASS_NAME, 'krw').text)
+
+    #print(ADA_USD)
+
+driver.quit()
