4ff31c5c2241bca64f7577ca3a0e45cb8c0fb456
Dokyu Lee <leewan@Dokyuui-MacBookPro.local>
2022-07-06 23:58:00 +0900

    Remove unusable comment

--- jpg_prac.py
+++ jpg_prac.py
@@ -34,28 +34,19 @@ def main():
     driver.get(corn['url'])
     while(1):
         WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.XPATH,'//*[@id="collectionSearch"]' )))
-        #time.sleep(3)
         for s in size:
             print("==== {} ====".format(s))
-            #WebDriverWait(driver, 100).until(EC.presence_of_all_elements_located((By.ID,'collectionSearch' )))
             find_bar = driver.find_element(By.ID, 'collectionSearch')
             WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.ID,'asset-price')))
             driver.implicitly_wait(10)
             for t in tier:
                 txt = '{} land plot claim {}'.format(s, t)
                 find_bar.send_keys(txt)
-                #WebDriverWait(driver, 100).until(EC.element_to_be_selected((By.XPATH,'//*[@id="asset-price"]' )))
                 el = WebDriverWait(driver, 5).until(EC.staleness_of(driver.find_element(By.ID,'asset-price')))
-                #time.sleep(2)
-                #WebDriverWait(driver, 100).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,'#marketplace > div.infinite-scroll-component__outerdiv > div > div.styles_customLoaderContainer__REOt4')))
                 price = driver.find_element(By.ID, 'asset-price').text
-                #el = WebDriverWait(driver, 5).until(EC.invisibility_of_element_located((By.ID,'asset-price')))
                 for i in range(len(txt)):
                     find_bar.send_keys(Keys.BACKSPACE)
-                    #el = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR,'#marketplace > div.infinite-scroll-component__outerdiv > div > div.styles_customLoaderContainer__REOt4')))
                 print('{} : {}'.format(t, price))
-                #el = WebDriverWait(driver, 5).until(EC.invisibility_of_element_located((By.CSS_SELECTOR,'#marketplace > div.infinite-scroll-component__outerdiv > div > div.styles_customLoaderContainer__REOt4')))
-                #el = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.ID,'asset-price')))
         print("==============")
     driver.quit()
 
