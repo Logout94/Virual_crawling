7b8001df9018e4e49cced042818b09ee5989b5d9
Dokyu Lee <leewan@Dokyuui-MacBookPro.local>
2022-07-07 00:16:23 +0900

    Add explicit wait

--- jpg_prac.py
+++ jpg_prac.py
@@ -29,6 +29,9 @@ def main():
     driver = chromeOption()
     p_info = jsonLoad('NFT.json')
     corn = p_info['Projects']['Cornucopias']
+    pavia = p_info['Projects']['Pavia']
+    moon = p_info['Projects']['Moon']
+
     tier = corn['property']['tier']
     size = corn['property']['size']
     driver.get(corn['url'])
@@ -38,11 +41,11 @@ def main():
             print("==== {} ====".format(s))
             find_bar = driver.find_element(By.ID, 'collectionSearch')
             WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.ID,'asset-price')))
-            driver.implicitly_wait(10)
             for t in tier:
                 txt = '{} land plot claim {}'.format(s, t)
                 find_bar.send_keys(txt)
-                el = WebDriverWait(driver, 5).until(EC.staleness_of(driver.find_element(By.ID,'asset-price')))
+                WebDriverWait(driver, 100).until(EC.staleness_of(driver.find_element(By.ID,'asset-price')))
+                WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.ID,'asset-price')))
                 price = driver.find_element(By.ID, 'asset-price').text
                 for i in range(len(txt)):
                     find_bar.send_keys(Keys.BACKSPACE)
